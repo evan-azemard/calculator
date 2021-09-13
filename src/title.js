9ea5f8984d789ad8329e6779196c1d28869c4164
evan-azemard <evan-azemard@laplateforme.io>
2021-09-13 13:56:48 +0200

    job 1

---
+++ src/title.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const TheTitle = () => {
+        return (
+            <div className="TheTitle">
+                <h1>Calculator9000</h1>
+            </div>
+        );
+}
+export default TheTitle;
